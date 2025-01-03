1b9cab8bae7f9f4a61b34fbe227c0c9f79bdfcfe
KananiDarpanAshokbha <KananiDarpanAshokbha@VLPO98>
2025-01-03 17:27:24 +0530

    shifted the header to left

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { Task } from '../types';
 import { HeaderComponent } from "../header/header.component";
 import { RouterModule, RouterOutlet } from '@angular/router';
@@ -8,6 +8,7 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   imports: [HeaderComponent, RouterOutlet, RouterModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css',
+  encapsulation: ViewEncapsulation.None
 
 })
 export class AdminComponent {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component, computed, inject } from '@angular/core';
 import { AuthServiceService } from '../../services/auth-service.service';
 import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule, RouterLink],
+  imports: [RouterModule, RouterLink, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
